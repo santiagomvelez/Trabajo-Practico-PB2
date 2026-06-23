416149ab5a0def55364178e0eed8581410558688
santiagomvelez <santiagomvelez1002@gmail.com>
2026-06-23 19:36:26 -0300

    Test 1 y 2 y metodo en refugio

--- tpBasica2-RefugioAnimales/src/ar/edu/unlam/clases/com/Refugio.java
+++ tpBasica2-RefugioAnimales/src/ar/edu/unlam/clases/com/Refugio.java
@@ -1,12 +1,18 @@
 package ar.edu.unlam.clases.com;
+
+import java.util.HashMap;
+
+
 public class Refugio {
 	
 	private Integer id;
 	private String nombre;
+	private HashMap<Integer, Animal> animales;
 	
 	public Refugio(Integer id, String nombre) {
 		this.id= id;
 		this.nombre = nombre;
+		this.animales = new HashMap<>();
 	}
 
 	public Integer getId() {
@@ -16,7 +22,19 @@ public class Refugio {
 	public String getNombre() {
 		return nombre;
 	}
-	
-	
 
+	public Boolean registrarAnimal(Animal animal) {
+		// si no cargamos ningun dato en animal devuelve falso
+		if(animal == null) {
+			return false;
+		}
+		// si el codigo que esta registrado es 1 y el nuevo que queremos registrar es 1 devuelve falso
+		if (this.animales.containsKey(animal.getCodigo())) {
+			return false;
+		}
+		
+		// si ninguno de los dos casos anteriores no pasa se registra al animal y devuelve verdadero
+		this.animales.put(animal.getCodigo(), animal);
+		return true;
+	}
 }
--- tpBasica2-RefugioAnimales/test/ar/edu/unlam/test/com/RefugioTest.java
+++ tpBasica2-RefugioAnimales/test/ar/edu/unlam/test/com/RefugioTest.java
@@ -4,21 +4,50 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import ar.edu.unlam.clases.com.Animal;
+import ar.edu.unlam.clases.com.Perro;
 import ar.edu.unlam.clases.com.Refugio;
+import ar.edu.unlam.enums.com.Tamanio;
 
 public class RefugioTest {
 
 	@Test
 	public void dadoQueExisteUnRefugioSePuedeRegistrarUnAnimal() {
 		
+		// Instancio el objeto y creo un refugio
 		Refugio refugio = new Refugio(1, "Patitas");
 		
-		Animal perro = new Perro()
+		// instancio la clase Perro que es una hija de clase Animal
+		Animal perro = new Perro(1,"tony", "Labrador",3, Tamanio.MEDIANO);
 				
+		// Verificaion de registrar animal
 		Boolean registrarAnimal = refugio.registrarAnimal(perro);
 		
+		// Validacion que debe devolver TRUE
+		assertTrue(registrarAnimal);
+	}
+	
+	
+	@Test 
+	public void dadoQueExisteUnRefugioNoSePuedenRegistrarDosAnimalesConElMismoCodigo() {
+		
+		// Instancio el objeto y creo un refugio
+		Refugio refugio = new Refugio(1, "Patitas");
+		
+		/* instancio la clase Perro que es una hija de clase Animal
+		 *  Lo hago dos veces con el mismo id para verificarlo que no se pueda registrar
+		 */
+		Animal perro = new Perro(1,"tony", "Labrador",3, Tamanio.MEDIANO);
+		Animal perro2 = new Perro(1,"tony", "Labrador",3, Tamanio.MEDIANO);
+		
+		// Lo registro las dos veces una normal y que la otra devuelva el Boolean para hacer la valdidacion
+		refugio.registrarAnimal(perro);
+		boolean registrarAnimal2 = refugio.registrarAnimal(perro2);
 		
 		
+		// Validacion valores esperados que devuelva Falso para registrar y que traigan el codigo y es el mismo
+		assertFalse(registrarAnimal2);
+		assertEquals(perro.getCodigo(), perro2.getCodigo(), 0.01);
 		
 	}
 
