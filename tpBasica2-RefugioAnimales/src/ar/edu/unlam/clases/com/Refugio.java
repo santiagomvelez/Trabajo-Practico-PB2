41ca1a9959a79dc0ea5e624ea3e731f119f7d6ed
Nazarena <nazarenaaylenmolina@gmail.com>
2026-06-24 11:29:28 -0300

    Implementé el Test 2, 3 y 10 con sus respectivos métodos

--- tpBasica2-RefugioAnimales/src/ar/edu/unlam/clases/com/Refugio.java
+++ tpBasica2-RefugioAnimales/src/ar/edu/unlam/clases/com/Refugio.java
@@ -1,5 +1,6 @@
 package ar.edu.unlam.clases.com;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 
@@ -8,20 +9,19 @@ public class Refugio {
 	private Integer id;
 	private String nombre;
 	private HashMap<Integer, Animal> animales;
+	private ArrayList<Adoptante>adoptantes;
+	private ArrayList<Adopcion>adopciones;
+	
 	
 	public Refugio(Integer id, String nombre) {
 		this.id= id;
 		this.nombre = nombre;
 		this.animales = new HashMap<>();
+		this.adoptantes=new ArrayList<Adoptante>();
+		this.adopciones=new ArrayList<Adopcion>();
 	}
 
-	public Integer getId() {
-		return id;
-	}
 
-	public String getNombre() {
-		return nombre;
-	}
 
 	public Boolean registrarAnimal(Animal animal) {
 		// si no cargamos ningun dato en animal devuelve falso
@@ -37,4 +37,78 @@ public class Refugio {
 		this.animales.put(animal.getCodigo(), animal);
 		return true;
 	}
+
+	public Boolean registrarAdoptante(Adoptante adoptante) {
+		//Si el adoptante que le paso por parámetros NO es nulll, lo registra y agrega a la colección del refugio
+		if(adoptante!=null) {
+			this.adoptantes.add(adoptante);
+			return true;
+		}
+		return false;
+	}
+
+	public Boolean procesarAdopcion(Integer idAdopcion, Adoptante adoptante, Animal perro) {
+		//Si los dos objetyos que se ingresan no son null
+		if(adoptante!=null && perro!=null) {
+		//Creo una adopcion con sus respectivos parámetros y la agrego a mi lista de adopciones.	
+			Adopcion adopcion=new Adopcion(idAdopcion, adoptante, perro);
+			this.adopciones.add(adopcion);
+			return true;
+		}
+		return false;
+	}
+	
+
+	public Animal buscarAnimalPorCodigo(Integer codigo) {
+		//Recorro todos los animales de mi HashMap
+		for(Animal animal: animales.values()) {
+			//Comparo el código del animal con el código buscado
+			if(animal.getCodigo().equals(codigo)){
+				//Si lo encuentra devuelve el animal y sino null
+				return animal;
+			}
+		}
+		return null;
+	}
+	
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public HashMap<Integer, Animal> getAnimales() {
+		return animales;
+	}
+
+	public void setAnimales(HashMap<Integer, Animal> animales) {
+		this.animales = animales;
+	}
+	
+
+	public ArrayList<Adoptante> getAdoptantes() {
+		return adoptantes;
+	}
+
+	public void setAdoptantes(ArrayList<Adoptante> adoptantes) {
+		this.adoptantes = adoptantes;
+	}
+	
+	public ArrayList<Adopcion> getAdopciones() {
+		return adopciones;
+	}
+
+	public void setAdopciones(ArrayList<Adopcion> adopciones) {
+		this.adopciones = adopciones;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
--- tpBasica2-RefugioAnimales/test/ar/edu/unlam/test/com/RefugioTest.java
+++ tpBasica2-RefugioAnimales/test/ar/edu/unlam/test/com/RefugioTest.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import ar.edu.unlam.clases.com.Adoptante;
 import ar.edu.unlam.clases.com.Animal;
+import ar.edu.unlam.clases.com.Gato;
 import ar.edu.unlam.clases.com.Perro;
 import ar.edu.unlam.clases.com.Refugio;
 import ar.edu.unlam.enums.com.Tamanio;
@@ -18,13 +20,15 @@ public class RefugioTest {
 		Refugio refugio = new Refugio(1, "Patitas");
 		
 		// instancio la clase Perro que es una hija de clase Animal
-		Animal perro = new Perro(1,"tony", "Labrador",3, Tamanio.MEDIANO);
+		Animal perro = new Perro(1,"Tony", "Labrador",3, Tamanio.MEDIANO);
 				
 		// Verificaion de registrar animal
 		Boolean registrarAnimal = refugio.registrarAnimal(perro);
 		
 		// Validacion que debe devolver TRUE
+		//Valido que se agregue a la colección
 		assertTrue(registrarAnimal);
+		assertEquals(1, refugio.getAnimales().size());
 	}
 	
 	
@@ -45,10 +49,83 @@ public class RefugioTest {
 		boolean registrarAnimal2 = refugio.registrarAnimal(perro2);
 		
 		
-		// Validacion valores esperados que devuelva Falso para registrar y que traigan el codigo y es el mismo
+		// Validacion valores esperados que devuelva Falso para registrar y que traigan el codigo y es el mismo y que se guarde solo uno
 		assertFalse(registrarAnimal2);
 		assertEquals(perro.getCodigo(), perro2.getCodigo(), 0.01);
-		
+		assertEquals(1, refugio.getAnimales().size());
 	}
+	
+	@Test
+	public void dadoQueExisteUnRefugioSePuedeRegistrarUnAdoptante() {
+		// Instancio el objeto y creo un refugio
+		Refugio refugio = new Refugio(1, "Patitas");
+	
+		// Instancio el objeto y creo un adoptante
+		Adoptante adoptante=new Adoptante(46952188, "Nazarena", "Molina", 30);
+		
+		//Registro al adoptante para validar que se guarda correctamente
+		Boolean resultado=refugio.registrarAdoptante(adoptante);
+		
+		//Valido que se registre y que se guarde en la colección de los adoptantes del refugio
+		assertTrue(resultado);
+		assertEquals(1, refugio.getAdoptantes().size());
+	}
+
+	
+	@Test
+	public void dadoQueExisteUnRefugioSePuedeProcesarUnaAdopcion() {
+		// Instancio el objeto y creo un refugio
+		Refugio refugio = new Refugio(1, "Patitas");
+		
+		// Instancio el objeto y creo un adoptante
+		Adoptante adoptante=new Adoptante(46952188, "Nazarena", "Molina", 30);
+		
+		// instancio la clase Perro que es una hija de clase Animal
+		Animal perro = new Perro(1,"Tony", "Labrador",3, Tamanio.MEDIANO);
+
+		//Registro adoptante
+		refugio.registrarAdoptante(adoptante);
+		
+		// Registro animal
+	    refugio.registrarAnimal(perro);
+	    
+	    // Registro adopcion
+	    Boolean resultado=refugio.procesarAdopcion(1, adoptante, perro);
+	    
+	    // Valido que la adopcion se procese correctamente y se guarde en la colección de adopciones realizadas
+	    assertTrue(resultado);
+	    assertEquals(1, refugio.getAdopciones().size());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@Test
+	public void  dadoQueExisteUnAnimalRegistradoSeLoPuedeBuscarPorCodigo() {
+		// Instancio el objeto y creo un refugio
+		Refugio refugio = new Refugio(1, "Patitas");
+		Integer codigoAnimal=1;
+		
+		// Instancio la clase Gato que es una hija de clase Animal
+		Animal gato = new Gato(codigoAnimal, "Golden", "Siamés", 3);
+		
+		// Registro animal
+		refugio.registrarAnimal(gato);
+		
+		//Busco el animal por el código
+		Animal animalEncontrado=refugio.buscarAnimalPorCodigo(codigoAnimal);
+		
+		//Valido que el animal que me devuelve sea igual al animal encontrado
+		assertEquals(animalEncontrado, gato);
+	}
+	
+	
+	
 
 }
