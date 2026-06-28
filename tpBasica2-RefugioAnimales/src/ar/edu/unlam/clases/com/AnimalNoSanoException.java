8dd58e4c781e1572b01b615990271442e19f839a
mia <mia@192.168.0.11>
2026-06-28 14:55:43 -0300

    cambio de list a hashmap en adoptantes.implementacion de metodos 5 y 6. cambio y arreglo de metodo procesar adopcion. se necesita verificar los tests de naza debido a los cambios.

---
+++ tpBasica2-RefugioAnimales/src/ar/edu/unlam/clases/com/AnimalNoSanoException.java
@@ -0,0 +1,8 @@
+package ar.edu.unlam.clases.com;
+
+public class AnimalNoSanoException extends Exception {
+
+    public AnimalNoSanoException() {
+        super("El animal no está sano.");
+    }
+}
